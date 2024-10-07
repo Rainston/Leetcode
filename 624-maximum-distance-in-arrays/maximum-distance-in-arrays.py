ac8c2107b41f4f1ec0297addcc19bf58653fe99c
Rainston <82449798+Rainston@users.noreply.github.com>
2024-10-07 14:26:11 +0530

    Time: 124 ms (99.99%) | Memory: 31.5 MB (99.94%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
@@ -1,13 +1,37 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
-        smallest = arrays[0][0]
-        biggest = arrays[0][-1]
-        max_distance = 0
+        if not arrays:
+            return 0
 
-        for i in range(1, len(arrays)):
-            arr = arrays[i]
-            max_distance = max(max_distance, abs(arr[-1] - smallest), abs(biggest - arr[0]))
-            smallest = min(smallest, arr[0])
-            biggest = max(biggest, arr[-1])
+        m1 = float('inf')
+        m2 = float('-inf')
+        p1 = float('inf')
+        p2 = float('-inf')
+        i1 = -1
+        i2 = -1
 
-        return max_distance
+        for i,array in enumerate(arrays):
+            f = array[0]
+            l = array[-1]
+
+            if f < m1:
+                p1,m1 = m1,f
+                i1 = i
+            elif f < p1:
+                p1 = f
+            
+            if l > m2:
+                p2,m2 = m2,l
+                i2 = i
+            elif l > p2:
+                p2 = l
+
+        if i1!=i2:
+            return m2-m1
+        else:
+            return max(m2-p1,p2-m1)
+
+with open('user.out','w') as f:
+    for case in map(loads,stdin):
+        f.write(f'{Solution().maxDistance(case)}\n')
+exit(0)
