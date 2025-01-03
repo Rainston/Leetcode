e3a3387095699432319281838db7aa6889a2ffb8
Rainston <82449798+Rainston@users.noreply.github.com>
2025-01-04 00:59:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (15.25%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,3 +1,16 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        def asd(x,n):
+            if x==0:
+                return 0
+            if n==0:
+                return 1
+            res = asd(x,n//2)
+            res=res*res
+            if n%2==1:
+                return res*x
+            return res
+        ans=asd(x,abs(n))
+        if n>=0:
+            return ans
+        return 1/ans
